12bb9b3972e2d4381b546b51d453289dfed64193
paolocarta82-eng <paolo.carta.82@gmail.com>
2026-09-08 15:24:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='copertura-v1';
+const F=['./','./index.html','./support.js','./rete-grafo.js','./ios-frame.jsx','./manifest.webmanifest','./icona.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))))});
